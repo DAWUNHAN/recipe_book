17a2d62d90a3b109ae92a9ced260021c630c2eac
DAWUNHAN <dawun.han@gmail.com>
2020-09-27 14:33:44 +1000

    Search function

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,18 +1,39 @@
 "use strict";
-import { fetchRecipe } from "../db";
+
+import dotenv from "dotenv";
+dotenv.config();
+const fetch = require("node-fetch");
+const key = process.env.RECIPE_API_KEY;
 
 export const home = async (req, res) => {
-  const recipes = await fetchRecipe();
-  console.log(recipes);
-  res.render("home", { pageTitle: "Home", recipes });
+  try {
+    const url = `https://api.spoonacular.com/recipes/random?number=2&apiKey=${key}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    let recipes = data.recipes;
+    console.log("good");
+    // console.log(recipes);
+    res.render("home", { pageTitle: "Home", recipes });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", recipes });
+  }
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-  // const searchingBy = req.query.term;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  try {
+    const url = `https://api.spoonacular.com/recipes/search?query=${searchingBy}&number=2&apiKey=${key}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    let searchResult = data.results;
+    res.render("search", { pageTitle: "Search", searchingBy, searchResult });
+  } catch (error) {
+    console.log(error);
+    res.redirect(routes.home);
+  }
 };
 
 export const recipe = (req, res) =>
--- db.js
+++ db.js
@@ -1,33 +1,34 @@
-import dotenv from "dotenv";
-dotenv.config();
-const fetch = require("node-fetch");
-const key = process.env.RECIPE_API_KEY;
+// import dotenv from "dotenv";
+// dotenv.config();
+// const fetch = require("node-fetch");
+// const key = process.env.RECIPE_API_KEY;
 
-// export const recipes = [
-//   {
-//     id: 1,
-//     title: "kimchi",
-//     category: "korean",
-//   },
-//   {
-//     id: 2,
-//     title: "pizza",
-//     category: "italian",
-//   },
-// ];
+// export const fetchRecipe = async (req, res) => {
+//   try {
+//     const url = `https://api.spoonacular.com/recipes/random?number=2&apiKey=${key}`;
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     let recipes = data.recipes;
+//     console.log("good");
+//     return recipes;
+//     // console.log(recipes);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-export const fetchRecipe = async (req, res) => {
-  try {
-    const url = `https://api.spoonacular.com/recipes/random?number=2&apiKey=${key}`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    // let recipes = data.recipes[0];
-    let recipes = data.recipes;
-    console.log("good");
-    return recipes;
-    // console.log(recipes);
-  } catch (error) {
-    console.log(error);
-  }
-};
+// export const searchRecipe = async (req, res) => {t
+//   console.log(req);
+//   try {
+//     const url = `https://api.spoonacular.com/recipes/search?query=${searchingBy}&number=2&apiKey=${key}`;
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     // console.log(data);
+//     let searchResult = data.results;
+//     // console.log("search");
+//     return searchResult;
+//     // console.log(recipes);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
